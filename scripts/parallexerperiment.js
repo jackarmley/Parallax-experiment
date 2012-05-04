d1a75b1f13d855b5c64736ab6dbab636a41fe714
jackarmley <jack_armley@yahoo.co.uk>
2012-05-04 14:14:09 +0800

    Initial file adding and such

---
+++ scripts/parallexerperiment.js
@@ -0,0 +1,26 @@
+/*==========
+[=1: Load libraries]
+===========*/  
+
+/*==========
+[=2: Custom functions]
+===========*/ 
+
+(function(window,undefined ) {
+	var sitescripts = {
+		functiongroup:{
+			functionname: function(){
+			}
+		},
+		functiongroup:{
+			functionname: function(){
+			}
+		}
+	} 
+window.sitescripts = sitescripts;
+})(window);	  
+	
+
+$(document).ready(function() {
+	sitescripts.functiongroup.functionname()		 
+});
