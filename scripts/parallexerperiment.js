1855a48bf3ba37040075ddf796d7f8c984aa36af
Lou Macleod <lou@loucloud.co.uk>
2012-07-06 16:24:46 +0100

    changes

--- scripts/parallexerperiment.js
+++ scripts/parallexerperiment.js
@@ -1,26 +1,16 @@
-/*==========
-[=1: Load libraries]
-===========*/  
-
-/*==========
-[=2: Custom functions]
-===========*/ 
-
-(function(window,undefined ) {
-	var sitescripts = {
-		functiongroup:{
-			functionname: function(){
-			}
-		},
-		functiongroup:{
-			functionname: function(){
-			}
+var scroll = {
+	scroller: function(){
+		$(window).on("scroll",function(e){
+			parallaxScroll();
+		});
+		
+		function parallaxScroll(){
+			var scrolled = $(window).scrollTop();
+			$("#bg-1").css('top',(500-(scrolled*.25))+'px');
+			$("#bg-2").css('top',(530-(scrolled*.35))+'px');
+			
 		}
-	} 
-window.sitescripts = sitescripts;
-})(window);	  
-	
+	}
+}
 
-$(document).ready(function() {
-	sitescripts.functiongroup.functionname()		 
-});
+scroll.scroller();
